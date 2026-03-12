552a3878103be7d907c7ddd7561eaefcfbd7d77e
unknown <atolerohan2003@gmail.com>
2026-03-12 15:41:12 +0530

    Updated Spring Boot journalApp project

---
+++ journalApp/journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,27 @@
+package net.engineeringdigest.journalApp.entity;
+
+import java.time.LocalDateTime;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.mongodb.lang.NonNull;
+
+import lombok.Data;
+
+@Document(collection = "journal_entries")
+@Data
+public class JournalEntry {
+
+	@Id
+	private ObjectId id;
+	
+	@NonNull
+	private String title;
+	
+	private String content;
+	
+	private LocalDateTime date;
+	
+}
